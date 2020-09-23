cafa232c2048f5df1ef842402db89af80b34c9da
Carlos A Rodriguez <crodri81@gmail.com>
2020-09-23 15:01:37 -0300

    Initial Commit

---
+++ SimpleCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCollectionView
+//
+//  Created by Carlos Rodriguez on 9/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
